a0374e7cdf9d10ef92206ae8d22a67654c34e979
AzureAD\ysakai <y.sakai@arent3d.com>
2023-09-01 02:37:43 +0900

    3Dビューリボンを作成しました

---
+++ revit_family_viewer/Command/CreateRibbonTabCommand.cs
@@ -0,0 +1,33 @@
+﻿using System ;
+using System.Collections.Generic ;
+using Autodesk.Revit.DB ;
+using Autodesk.Revit.UI ;
+using Autodesk.UI.Windows ;
+using System.Linq ;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes ;
+
+namespace revit_family_viewer.Command
+{
+  [Transaction(TransactionMode.ReadOnly)]
+  public class CreateRibbonTabCommand : IExternalCommand
+  {
+    public Result Execute( ExternalCommandData commandData, ref string message, ElementSet elements )
+    {
+      UIApplication uiapp = commandData.Application ;
+      Application app = uiapp.Application ;
+
+      try {
+        string tabName = "3Dビュー" ;
+        uiapp.CreateRibbonTab( tabName ) ;
+        
+        return Result.Succeeded;
+      }
+      catch ( Exception e ) {
+        message = e.Message ;
+        return Result.Failed ;
+      }
+      
+    }
+  }
+}
